b1768caca0daa2d81555f1fe7acd312742401bd2
Marios011 <mariobardha@hotmail.com>
2026-08-13 04:00:36 +0300

    restaurant ordering system

--- src/main/java/com/restaurant/orderservice/model/OrderEntity.java
+++ src/main/java/com/restaurant/orderservice/model/OrderEntity.java
@@ -1,9 +1,9 @@
 package com.restaurant.orderservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class OrderEntity {
